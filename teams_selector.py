5052a425b07198a5ab64fa2856fdc05843f00ea7
WnndGws <WnndGws@users.noreply.github.com>
2020-05-11 05:38:11 +0800

    Dont really know where to next

--- teams_selector.py
+++ teams_selector.py
@@ -6,9 +6,36 @@ then saves choices into a bracket
 import csv
 import random
 
-with open('teams.csv', newline='') as csvfile:
-    csvreader = csv.DictReader(csvfile)
-    random_team_number = random.randint(1,120)
-    for row in csvreader:
-        if int(row['Index']) == random_team_number:
-            print(row)
+import click
+
+def set_options():
+    with open('teams.csv', newline='') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        random_team_number_1 = random.randint(1,120)
+        random_team_number_2 = random.randint(1,120)
+        for row in csvreader:
+            if int(row['Index']) == random_team_number_1:
+                option_1 = f'{row["Name"]} ({row["League"]})({row["OverAll"]})'
+                team_1 = row["Name"]
+            elif int(row['Index']) == random_team_number_2:
+                option_2 = f'{row["Name"]} ({row["League"]})({row["OverAll"]})'
+                team_2 = row["Name"]
+    return (team_1, team_2)
+
+team_1 = set_options()[0]
+team_2 = set_options()[1]
+
+@click.command()
+@click.option('--choice', prompt=True, type=click.Choice(["1", "2"]))
+def choice_made(choice):
+    if choice == "1":
+        team = team_1
+    else:
+        team = team_2
+
+    print(team)
+
+if __name__ == "__main__":
+    print(f'[1] {team_1}')
+    print(f'[2] {team_2}')
+    choice_made()
