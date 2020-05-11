28375219193d3c47f28b0c38fc73f85bc04bfe93
WnndGws <WnndGws@users.noreply.github.com>
2020-05-11 18:11:08 +0800

    Have gotten to a point where random matchups are created at least

--- teams_selector.py
+++ teams_selector.py
@@ -6,36 +6,52 @@ then saves choices into a bracket
 import csv
 import random
 
-import click
+USER = input("Who is selecting?: ")
+LOOPS = ''
+while LOOPS not in ['4', '8', '16']:
+    LOOPS = input('How many teams will you need? [4][8][16]: ')
 
-def set_options():
+def team_selector():
+    '''This just chooses two random teams from the csv file'''
     with open('teams.csv', newline='') as csvfile:
         csvreader = csv.DictReader(csvfile)
-        random_team_number_1 = random.randint(1,120)
-        random_team_number_2 = random.randint(1,120)
+        random_team_number_1 = random.randint(1, 120)
+        random_team_number_2 = random.randint(1, 120)
         for row in csvreader:
             if int(row['Index']) == random_team_number_1:
-                option_1 = f'{row["Name"]} ({row["League"]})({row["OverAll"]})'
-                team_1 = row["Name"]
+                team_1_name = row["Name"]
+                team_1_info = f'{row["Name"]} ({row["League"]})({row["OverAll"]})'
             elif int(row['Index']) == random_team_number_2:
-                option_2 = f'{row["Name"]} ({row["League"]})({row["OverAll"]})'
-                team_2 = row["Name"]
-    return (team_1, team_2)
+                team_2_name = row["Name"]
+                team_2_info = f'{row["Name"]} ({row["League"]})({row["OverAll"]})'
+    return (team_1_name, team_1_info, team_2_name, team_2_info)
 
-team_1 = set_options()[0]
-team_2 = set_options()[1]
-
-@click.command()
-@click.option('--choice', prompt=True, type=click.Choice(["1", "2"]))
-def choice_made(choice):
-    if choice == "1":
-        return(team_1)
+team_choices = []
+def team_choice():
+    '''This is where the user chooses one or the other'''
+    team_options = team_selector()
+    print(f'Option [1]: {team_options[1]}')
+    print(f'Option [2]: {team_options[3]}')
+    user_choice = ''
+    while user_choice not in ['1', '2']:
+        user_choice = input("Choose [1] or [2]: ")
+    if user_choice == "1":
+        print(f'You have chosen {team_options[0]}\n')
+        team_choices.append([team_options[0]])
     else:
-        return(team_2)
+        print(f'You have chosen {team_options[2]}\n')
+        team_choices.append([team_options[2]])
 
+def main():
+    '''Selects the correct amount of teams, and saves them'''
+    loops = 0
+    while loops < int(LOOPS):
+        team_choice()
+        loops += 1
+
+    with open(f'{USER}_choices.csv', 'w') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        csvwriter.writerows(team_choices)
 
 if __name__ == "__main__":
-    print(f'[1] {team_1}')
-    print(f'[2] {team_2}')
-    team = choice_made()
-    print(team)
+    main()
